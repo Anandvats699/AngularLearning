7f02d503bc9160d6867e28f8db42c61f7c770526
Anandvats699 <Anandvats699>
2023-03-10 14:44:40 +0530

    Initial commit by aj

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { HostComponent } from './host/host.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    HostComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
